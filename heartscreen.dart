be07909b7f03a7b26a4e15a87279249bc2462c6b
Nandini Dnyaneshwar Vaijawade <163897805+NandiniV-20@users.noreply.github.com>
2025-09-06 23:46:28 +0530

    like screen

---
+++ heartscreen.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:instagram/Screen/Heart/Following/Followingscreen.dart';
+import 'package:instagram/Screen/Heart/You/youscreen.dart';
+
+class HeartScreen extends StatelessWidget{
+  const HeartScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return DefaultTabController(length: 2,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              unselectedLabelColor: Colors.grey,
+              labelColor: Colors.white,
+              indicatorColor: Colors.white,
+                indicatorSize: TabBarIndicatorSize.tab,
+                tabs: [
+              Tab(text: "Following",),
+              Tab(text: "You",)
+            ]),
+          ),
+          body:TabBarView(children: [
+            FollowingScreen(),
+            YouScreen()
+          ]),
+        ));
+  }
+}
