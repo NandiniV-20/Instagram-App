0a4da332252589c378c7a366a526788a8b8bdcca
Nandini Dnyaneshwar Vaijawade <163897805+NandiniV-20@users.noreply.github.com>
2025-09-06 23:47:12 +0530

    Your activity screen

---
+++ youscreen.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class YouScreen extends StatelessWidget{
+  const YouScreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      body:Center(child: Text("You Screen"),)
+    );
+  }
+
+}
